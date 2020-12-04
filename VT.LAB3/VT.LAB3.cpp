124e5993f1efa42367ff54d7af97876b35b226ba
Cogot' Drakona <vlad140201@gmail.com>
2020-12-05 04:40:11 +0700

    Initial commit

---
+++ VT.LAB3/VT.LAB3.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL, "ru");
+    __int16 A = 350, B = 750;
+    __int32 C = 1500;
+    int D = 0; // D = (A * B) - C
+    _asm {
+        mov AX, A;
+        imul B;
+        SUB AX, word ptr C;
+        SBB DX, word ptr C + 2;
+        mov word ptr D, AX;
+        mov word ptr D + 2, DX;
+    }
+
+    cout << "D = (" << A << " * " << B << ") - " << C << endl;
+    cout << "D = " << D << endl;
+     
+    __int16 E = 0; // E = ( C / B ) + A
+    _asm {
+        mov AX, word ptr C;
+        mov DX, word ptr C + 2;
+        idiv B;
+        ADD AX, A;
+        mov word ptr E, AX;   
+    }
+
+    cout << "E = (" << C << " / " << B << ") + " << A << endl;
+    cout << "E = " << E << endl;
+    return 0;
+}
+
